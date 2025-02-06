6df01166c94460b30b0bc78f9db4e99753d1f173
YanoMikito <mikiy@OITTGLfBk001.>
2025-02-06 11:46:39 +0900

    a

--- src/simulator/agent.py
+++ src/simulator/agent.py
@@ -4,7 +4,7 @@ import copy, sys, math
 import numpy as np
 import pandas as pd
 
-from robotic_system.core import RobotState, ModuleState, RobotPerformanceAttributes
+from src.robotic_system.core import RobotState, ModuleState, RobotPerformanceAttributes
 
 prob = 0.005
 
